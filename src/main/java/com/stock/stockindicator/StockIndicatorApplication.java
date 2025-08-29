ce9c5f6b11896c65b788eb1976c1d5b57a0ad5f3
Krish Newatia <krish.n.newatia@oracle.com>
2025-08-30 02:07:26 +0530

    Beginning

---
+++ src/main/java/com/stock/stockindicator/StockIndicatorApplication.java
@@ -0,0 +1,13 @@
+package com.stock.stockindicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockIndicatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockIndicatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stock/stockindicator/StockIndicatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.stockindicator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockIndicatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
