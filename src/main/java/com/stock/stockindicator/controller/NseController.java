c5ef8b3a413ff827a6cadc97e95902a5caf1dd98
Krish Newatia <krish.n.newatia@oracle.com>
2025-09-03 20:44:23 +0530

    refactor

--- src/main/java/com/stock/stockindicator/controller/NseController.java
+++ src/main/java/com/stock/stockindicator/controller/NseController.java
@@ -1,7 +1,6 @@
 package com.stock.stockindicator.controller;
 
 import com.stock.stockindicator.service.NseService;
-import com.stock.stockindicator.utils.cookie.GenerateNewCookie;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,21 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class NseController {
 
     private final NseService nseService;
-    private final GenerateNewCookie generateNewCookie;
 
-    public NseController(NseService nseService, GenerateNewCookie generateNewCookie) {
+    public NseController(NseService nseService) {
         this.nseService = nseService;
-        this.generateNewCookie = generateNewCookie;
+    }
+
+    @GetMapping("/chart")
+    public String getChart() {
+        return "enter the stock ticker in the field";
     }
 
     @GetMapping("/chart/{symbol}")
     public String getChart(@PathVariable String symbol) {
         return nseService.getChartData(symbol);
     }
-
-    @GetMapping("/finalcookie")
-    public String getFinalCookie() {
-        return generateNewCookie.returnNewCookie();
-    }
-
 }
---
+++ src/main/java/com/stock/stockindicator/controller/tests/test.java
@@ -0,0 +1,11 @@
+package com.stock.stockindicator.controller.tests;
+
+public class test {
+    int marks =30;
+    public static void main(String[] args) {
+        test test = new test();
+        test test2 = new test();
+        test2.marks = 3;
+        System.out.println(test.marks);
+    }
+}
